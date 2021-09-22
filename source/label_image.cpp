23676739b52b232fd4d8df4a766f57b4915bbba8
marcin-ch <marcin.chelminski@futureelectronics.com>
2021-09-22 09:53:59 +0200

    Update emWin related code

--- source/label_image.cpp
+++ source/label_image.cpp
@@ -18,6 +18,10 @@ limitations under the License.
 #include "clock_config.h"
 #include "timer.h"
 
+#include "GUI.h"
+#include "emwin_support.h"
+#include "fsl_gpt.h"
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -34,6 +38,86 @@ limitations under the License.
 #include "stopwatch_image.h"
 #include "mobilenet_v1_0.25_128_quant_model.h"
 #include "labels.h"
+
+/* Initialize the LCD_DISP. */
+void BOARD_InitLcd(void)
+{
+    volatile uint32_t i = 0x100U;
+
+    gpio_pin_config_t config = {
+        kGPIO_DigitalOutput,
+        0,
+    };
+
+    /* Reset the LCD. */
+    GPIO_PinInit(LCD_DISP_GPIO, LCD_DISP_GPIO_PIN, &config);
+
+    GPIO_WritePinOutput(LCD_DISP_GPIO, LCD_DISP_GPIO_PIN, 0);
+
+    while (i--)
+    {
+    }
+
+    GPIO_WritePinOutput(LCD_DISP_GPIO, LCD_DISP_GPIO_PIN, 1);
+
+    /* Backlight. */
+    config.outputLogic = 1;
+    GPIO_PinInit(LCD_BL_GPIO, LCD_BL_GPIO_PIN, &config);
+
+    /*Clock setting for LPI2C*/
+    CLOCK_SetMux(kCLOCK_Lpi2cMux, LPI2C_CLOCK_SOURCE_SELECT);
+    CLOCK_SetDiv(kCLOCK_Lpi2cDiv, LPI2C_CLOCK_SOURCE_DIVIDER);
+}
+
+void BOARD_InitLcdifPixelClock(void)
+{
+    /*
+     * The desired output frame rate is 60Hz. So the pixel clock frequency is:
+     * (480 + 41 + 4 + 18) * (272 + 10 + 4 + 2) * 60 = 9.2M.
+     * Here set the LCDIF pixel clock to 9.3M.
+     */
+
+    /*
+     * Initialize the Video PLL.
+     * Video PLL output clock is OSC24M * (loopDivider + (denominator / numerator)) / postDivider = 93MHz.
+     */
+    clock_video_pll_config_t config = {
+        .loopDivider = 31,
+        .postDivider = 8,
+        .numerator   = 0,
+        .denominator = 0,
+    };
+
+    CLOCK_InitVideoPll(&config);
+
+    /*
+     * 000 derive clock from PLL2
+     * 001 derive clock from PLL3 PFD3
+     * 010 derive clock from PLL5
+     * 011 derive clock from PLL2 PFD0
+     * 100 derive clock from PLL2 PFD1
+     * 101 derive clock from PLL3 PFD1
+     */
+    CLOCK_SetMux(kCLOCK_LcdifPreMux, 2);
+
+    CLOCK_SetDiv(kCLOCK_LcdifPreDiv, 4);
+
+    CLOCK_SetDiv(kCLOCK_LcdifDiv, 1);
+}
+
+void BOARD_InitGPT(void)
+{
+    gpt_config_t gptConfig;
+
+    GPT_GetDefaultConfig(&gptConfig);
+
+    gptConfig.enableFreeRun = true;
+    gptConfig.divider       = 3000;
+
+    /* Initialize GPT module */
+    GPT_Init(EXAMPLE_GPT, &gptConfig);
+    GPT_StartTimer(EXAMPLE_GPT);
+}
 
 #define LOG(x) std::cout
 
@@ -195,21 +279,30 @@ void RunInference(Settings* s) {
 
 int main(void)
 {
-  tflite::label_image::Settings s;
+tflite::label_image::Settings s;
 
-  /* Init board hardware */
-  BOARD_ConfigMPU();
-  BOARD_InitPins();
-  BOARD_BootClockRUN();
-  BOARD_InitDebugConsole();
+/* Init board hardware */
+BOARD_ConfigMPU();
+BOARD_InitPins();
+BOARD_InitI2C1Pins();
+BOARD_InitSemcPins();
+BOARD_BootClockRUN();
+BOARD_InitLcdifPixelClock();
+BOARD_InitDebugConsole();
+BOARD_InitLcd();
+BOARD_InitGPT();
 
-  InitTimer();
+InitTimer();
 
-  std::cout << "Label image example using a TensorFlow Lite model\r\n";
+GUI_Init();
 
-  tflite::label_image::RunInference(&s);
+std::cout << "Label image example using a TensorFlow Lite model\r\n";
 
-  std::flush(std::cout);
+GUI_DispStringAt("emWin TEST!", 0, 0);
 
-  for (;;) {}
+tflite::label_image::RunInference(&s);
+
+std::flush(std::cout);
+
+for (;;) {}
 }
